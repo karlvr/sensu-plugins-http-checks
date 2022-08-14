c158a1e670cba26084ab1219a15e732c84625db7
Karl von Randow <karl@cactuslab.com>
2022-08-15 10:25:05 +1200

    Initial import

---
+++ bin/check-http.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+#
+#   check-http
+#
+# DESCRIPTION:
+#   Takes either a URL or a combination of host/path/port/ssl, and checks for
+#   a 200 response (that matches a pattern, if given). Can use client certs.
+#
+# OUTPUT:
+#   plain text
+#
+# PLATFORMS:
+#   Linux
+#
+# USAGE:
+#   #YELLOW
+#
+# NOTES:
+#
+# LICENSE:
+#   Copyright 2015 Karl von Randow <karl@xk72.com>
+#   Released under the same terms as Sensu (the MIT license); see LICENSE
+#   for details.
+#
+
+set -u
+
+URL=
+PATTERN=
+REDIRECTOK=
+
+while getopts ":u:q:r" opt; do
+  case $opt in
+	u)
+	  URL="$OPTARG"
+	  ;;
+	q)
+	  PATTERN="$OPTARG"
+	  ;;
+	r)
+	  REDIRECTOK="true"
+	  ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+if [ "x$URL" == "x" ]; then
+	echo "CheckHTTP UNKNOWN: Missing -u url option"
+	exit 3
+fi
+
+WGET=/usr/bin/wget
+
+ARGS=
+
+if [ "x$REDIRECTOK" != "xtrue" ]; then
+	ARGS="$ARGS --max-redirect 0"
+fi
+ARGS="$ARGS -nv -O- $URL"
+
+ERROR_LOG=`mktemp --tmpdir check-http.XXXXXXXXXX.tmp`
+
+OUTPUT=$(eval $WGET $ARGS 2>"$ERROR_LOG")
+STATUS=$?
+
+if [ $STATUS == 8 ]; then
+	echo "CheckHTTP WARNING: Server error: $URL"
+	cat "$ERROR_LOG"
+	rm -f "$ERROR_LOG"
+	exit 1
+elif [ $STATUS != 0 ]; then
+	echo "CheckHTTP CRITICAL: Failed to request: $URL"
+	cat "$ERROR_LOG"
+	rm -f "$ERROR_LOG"
+	exit 2
+elif [ "x$PATTERN" != "x" ]; then
+	echo "$OUTPUT" | grep "$PATTERN" 2>/dev/null >/dev/null
+	if [ $? != 0 ]; then
+		echo "CheckHTTP CRITICAL: Pattern mismatch: $URL"
+		rm -f "$ERROR_LOG"
+		exit 2
+	else
+		echo "CheckHTTP OK: Matched pattern: $URL"
+		rm -f "$ERROR_LOG"
+		exit 0
+	fi
+else
+	echo "CheckHTTP OK"
+	rm -f "$ERROR_LOG"
+	exit 0
+fi
---
+++ bin/check-https.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+set -u
+
+HOST=
+PORT=443
+EXPIRY=
+EXPIRY_CRITICAL=
+SERVER_NAME=
+
+while getopts ":h:p:s:e:E:" opt; do
+  case $opt in
+	h)
+	  HOST="$OPTARG"
+	  ;;
+	p)
+	  PORT="$OPTARG"
+	  ;;
+	s)
+	  SERVER_NAME="$OPTARG"
+	  if [ -z "$HOST" ]; then
+	  	HOST="$OPTARG"
+	  fi
+	  ;;
+	e)
+	  EXPIRY="$OPTARG"
+	  ;;
+	E)
+	  EXPIRY_CRITICAL="$OPTARG"
+	  ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+if [ "x$HOST" == "x" ]; then
+	echo "CheckHTTPS UNKNOWN: Missing -h host option"
+	exit 3
+fi
+
+if [ "x$PORT" == "x" ]; then
+	echo "CheckHTTPS UNKNOWN: Missing -p port option"
+	exit 3
+fi
+
+OPENSSL=/usr/bin/openssl
+
+if [ ! -x "$OPENSSL" ]; then
+	echo "CheckHTTPS UNKNOWN: Cannot find $OPENSSL"
+	exit 3
+fi
+
+if [ "x$SERVER_NAME" == "x" ]; then
+	OUTPUT=$($OPENSSL s_client -host "$HOST" -port "$PORT" < /dev/null 2>&1)
+else
+	OUTPUT=$($OPENSSL s_client -host "$HOST" -port "$PORT" -servername "$SERVER_NAME" < /dev/null 2>&1)
+fi
+STATUS=$?
+
+if [ $STATUS != 0 ]; then
+	echo "CheckHTTPS CRITICAL: Failed: $HOST:$PORT"
+	echo $OUTPUT
+	exit 2
+fi
+
+echo "CheckHTTPS OK: $HOST:$PORT"
+if [ "x$EXPIRY" != "x" -o "x$EXPIRY_CRITICAL" != "x" ]; then
+	NOT_AFTER=$(echo "$OUTPUT" | $OPENSSL x509 -enddate -noout | cut -d '=' -f 2)
+	if [ "x$NOT_AFTER" != "x" ]; then
+		NOT_AFTER_NUM=$(date +%s -d "$NOT_AFTER")
+		NOW=$(date +%s)
+		EXPIRY_DAYS=$((($NOT_AFTER_NUM - $NOW) / 86400))
+
+		if [ "x$EXPIRY_CRITICAL" != "x" -a $(($EXPIRY_DAYS < ($EXPIRY_CRITICAL + 1))) == 1 ]; then
+			echo "CheckHTTPS CRITICAL: Certificate expires in $EXPIRY_DAYS days: $HOST:$PORT"
+			exit 2
+		elif [ "x$EXPIRY" != "x" -a $(($EXPIRY_DAYS < ($EXPIRY + 1))) == 1 ]; then
+			echo "CheckHTTPS WARNING: Certificate expires in $EXPIRY_DAYS days: $HOST:$PORT"
+			exit 1
+		else
+			echo "Certificate expires in $EXPIRY_DAYS days"
+		fi
+	fi
+fi
+
+exit 0
